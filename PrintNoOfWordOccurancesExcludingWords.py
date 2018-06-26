9cd4f2df0e3c12c4f00e6aa575833d3e5ff3ebfb
malathi <malathi@malathis-MacBook-Pro.local>
2018-06-26 18:34:38 +0530

    Test commit 1

---
+++ PrintNoOfWordOccurancesExcludingWords.py
@@ -0,0 +1,22 @@
+
+fo = open("TestFO.txt", "wb")
+fo.write(raw_input("Enter any stings or numbers: "))
+fo.close()
+file = open("TestFO.txt")
+data = file.read()
+words = data.split()
+fo.close()
+
+unwanted_chars = ".,-_"
+wordfreq = {}
+for raw_word in words:
+    word = raw_word.strip(unwanted_chars)
+    if raw_word not in ('an', 'a', 'to', 'the', 'be', 'not'):
+        if word not in wordfreq:
+            wordfreq[word] = 0
+        wordfreq[word] += 1
+
+print wordfreq
+
+
+
